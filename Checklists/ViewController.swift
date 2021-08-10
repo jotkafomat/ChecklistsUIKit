a2abf3179d1af5ea2f106da30b8499b4b6064ae9
jotkafomat <k-j@xl.wp.pl>
2021-08-10 11:09:03 +0100

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Krzysztof Jankowski on 10/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
