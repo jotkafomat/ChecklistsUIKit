c77a52a985a5fc9cc74f53648cc971cb63dd0a9b
jotkafomat <k-j@xl.wp.pl>
2021-08-10 11:18:46 +0100

    change UIViewController to UITableViewController

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistViewController.swift
+//  Checklists
+//
+//  Created by Krzysztof Jankowski on 10/08/2021.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    
+}
+
