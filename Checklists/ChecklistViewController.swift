968f574eebc5f1abc29604d3feaccec4c89de783
jotkafomat <k-j@xl.wp.pl>
2021-08-11 15:11:16 +0100

    set navigationBarTitle to use large title

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -14,6 +14,8 @@ class ChecklistViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let item1 = ChecklistItem()
         item1.text = "Walk the dog"
         items.append(item1)
