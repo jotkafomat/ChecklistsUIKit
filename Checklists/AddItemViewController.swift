3ea7b31ca314a564fad8dd6900b8b21fe46d30e6
jotkafomat <k-j@xl.wp.pl>
2021-08-11 16:25:22 +0100

    add addItemViewController

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Krzysztof Jankowski on 11/08/2021.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
