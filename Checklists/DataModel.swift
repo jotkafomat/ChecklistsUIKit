1baefdb7bc591f0776c8390f64dc64380b2be8c1
jotkafomat <k-j@xl.wp.pl>
2021-08-19 12:24:24 +0100

    add the first-run experience

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -22,6 +22,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     // MARK: - Data Saving
@@ -63,11 +64,25 @@ class DataModel {
     }
     
     func registerDefaults() {
-        let dictionary = ["ChecklistIndex" : -1 ]
+        let dictionary = ["ChecklistIndex" : -1,
+        "FirstTime" : true ] as [String : Any]
         
         UserDefaults.standard.register(defaults: dictionary)
     }
     
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            
+            userDefaults.set(false, forKey: "FirstTime")
+        }
+    }
     
 
 }
