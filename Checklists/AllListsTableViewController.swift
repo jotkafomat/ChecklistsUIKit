6eaa2fb68253cc3016385c8677972cd78db49558
jotkafomat <k-j@xl.wp.pl>
2021-08-19 10:25:34 +0100

    saving app when it goes background

--- Checklists/AllListsTableViewController.swift
+++ Checklists/AllListsTableViewController.swift
@@ -14,28 +14,11 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
     var lists = [Checklist]()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        // Add placeholder data
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-        
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-        
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-        
-        list = Checklist(name: "To Do")
-        lists.append(list)
-        super.viewDidLoad()
-        
-        for list in lists {
-            let item = ChecklistItem()
-            item.text = "Item for \(list.name)"
-            list.items.append(item)
-        }
+        loadChecklists()
     }
     
     // MARK: - Table view data source
@@ -63,14 +46,14 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
     }
     
     override func tableView(
-      _ tableView: UITableView,
-      commit editingStyle: UITableViewCell.EditingStyle,
-      forRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
     ) {
-      lists.remove(at: indexPath.row)
-
-      let indexPaths = [indexPath]
-      tableView.deleteRows(at: indexPaths, with: .automatic)
+        lists.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
     }
     
     // MARK:- Navigation
@@ -132,5 +115,43 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
             }
         }
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - Data Saving
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(lists)
+            try data.write(
+                to: dataFilePath(),
+                options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding list array: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode(
+                    [Checklist].self,
+                    from: data)
+            } catch {
+                print("Error decoding list array: \(error.localizedDescription)")
+            }
+        }
     }
 }
