e9c9ade222656d07541badcd81075f51d8a5a420
jotkafomat <k-j@xl.wp.pl>
2021-08-19 20:13:03 +0100

    add icons and display icon

--- Checklists/ViewControllers/AllListsTableViewController.swift
+++ Checklists/ViewControllers/AllListsTableViewController.swift
@@ -70,6 +70,7 @@ class AllListsTableViewController: UITableViewController, ListDetailViewControll
         } else {
             cell.detailTextLabel!.text = count == 0 ? "All Done" : "\(count) Remaining"
         }
+        cell.imageView!.image = UIImage(named: checklist.iconName)
         return cell
     }
     
